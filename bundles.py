b8114da274744bac8774062c7b2fe42f097f3281
Etaminchik <43892972+Etaminchik@users.noreply.github.com>
2022-11-01 12:15:16 +0300

    Update bundles.py

--- bundles.py
+++ bundles.py
@@ -54,7 +54,7 @@ print('Logins: ', procent_logins, "% (", count_login, '/', count_logins, ')' )
 
 '''NAT'''
 
-cur.execute(f"select count(*) from {partition} where rawf_telco_code = {rawf_telco_code} and (rawf_client_address <<inet '10.0.0.0/8' OR rawf_client_address <<inet '172.16.0.0/12' or rawf_client_address  <<inet '192.168.0.0/16' or rawf_client_address  <<inet '100.64.0.0/10')  and not (rawf_server_address <<inet '10.0.0.0/8' OR rawf_server_address <<inet '172.16.0.0/12' or rawf_server_address  <<inet '192.168.0.0/16' or rawf_server_address <<inet '100.64.0.0/10')  and rawf_protocol_code != 65025 and rawf_nat_address is null")
+cur.execute(f"select count(*) from {partition} where rawf_telco_code = {rawf_telco_code} and (rawf_client_address <<inet '10.0.0.0/8' OR rawf_client_address <<inet '172.16.0.0/12' or rawf_client_address  <<inet '192.168.0.0/16' or rawf_client_address  <<inet '100.64.0.0/10')  and not (rawf_server_address <<inet '10.0.0.0/8' OR rawf_server_address <<inet '172.16.0.0/12' or rawf_server_address  <<inet '192.168.0.0/16' or rawf_server_address <<inet '100.64.0.0/10')  and rawf_protocol_code != 65025 and rawf_nat_address is not null")
 count_nat = cur.fetchall()[0][0]
 
 cur.execute(f"select count(*) from {partition} where rawf_telco_code = {rawf_telco_code} and (rawf_client_address <<inet '10.0.0.0/8' OR rawf_client_address <<inet '172.16.0.0/12' or rawf_client_address  <<inet '192.168.0.0/16' or rawf_client_address  <<inet '100.64.0.0/10')  and not (rawf_server_address <<inet '10.0.0.0/8' OR rawf_server_address <<inet '172.16.0.0/12' or rawf_server_address  <<inet '192.168.0.0/16' or rawf_server_address <<inet '100.64.0.0/10')")
